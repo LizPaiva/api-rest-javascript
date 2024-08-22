5e321117db76cfbb1a7d37656e05465be4e4a479
Lizeth Paiva <lizethpaiva2@gamil.com>
2024-08-22 12:27:16 -0500

    clase 03 entorno de desarrollo

---
+++ src/main.js
@@ -0,0 +1,2 @@
+
+console.log('API URL = https://api.themoviedb.org/3/movie/550?api_key=a978c50c25cab0e6e852fd99e3af6c31' + API_KEY);
---
+++ src/secrets.js
@@ -0,0 +1,1 @@
+const API_KEY = 'a978c50c25cab0e6e852fd99e3af6c31';
